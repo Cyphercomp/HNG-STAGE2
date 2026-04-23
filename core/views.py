e95f9560d1356f8ef47ccf7de6a40785fc6123e4
Cyphercomp <ndicunguyecedric@gmail.com>
2026-04-23 16:54:19 +0200

    added intelligence search

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from rest_framework.filters import SearchFilter
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.mixins import ListModelMixin
-from resst_framework.decorators import action
+from rest_framework.decorators import action
 from rest_framework import status
 from .models import Profile
 from .serializer import ProfileSerializer
